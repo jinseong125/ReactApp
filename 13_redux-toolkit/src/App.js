1dae88b72d499689e547c07580ea6d1a252d3d39
jinseong125 <jinseong125@gmail.com>
2025-10-20 11:24:51 +0900

    20251017

---
+++ 13_redux-toolkit/src/App.js
@@ -0,0 +1,56 @@
+/*
+  4. 컴포넌트
+    1) Redux Store의 상태를 읽기 위해서 react-redux의 useSelector() 훅을 사용합니다.
+    2) 상태를 변경하기 위해서 액션을 디스패치(전달)합니다.
+      (1) 액션 생성자들을 import 합니다.
+      (2) react-redux의 useDispatch() 훅을 사용합니다.
+*/
+
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './features/counterSlice';
+
+function App() {
+
+  // useSelector()
+  // 스토어에서 원하는 상태를 가져옵니다.
+  //  state.counter : 스토어에서 저장한 key
+  //  state.counter.number : 스토어에 저장한 value
+  const number = useSelector( state => state.counter.number );
+
+  // useDispatch()
+  // 디스패치 함수를 반환합니다.
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>현재 상태 number: {number}</h1>
+      {/*
+        increment() 호출 시 생성되는 액션 객체는 아래와 같습니다.
+        { type: "counter/increment" } - counterSlice.js 참고
+      */}
+      <button onClick={() => dispatch(increment())}> + </button>
+      <button onClick={() => dispatch(decrement())}> - </button>
+      <button onClick={() => dispatch(reset())}> reset </button>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+  동작 플로우 이해하기
+
+  1. 사용자가 + 버튼을 클릭합니다.
+
+  2. dispatch(increment()) 함수가 호출됩니다.
+
+  3. dispatch({ type: "counter/increment" }) 형태로 함수가 호출됩니다. (액션 타입 결정)
+
+  4. { type: "counter/increment" } 액션을 counterSlice.js의 리듀서로 전달합니다.
+
+  5. 리듀서는 전달된 액션 객체의 type에 따라 리듀서 함수((state) => state.number)를 호출합니다.
+
+  6. 스토어(store.js)의 상태가 업데이트 됩니다.
+
+  7. useSelector()가 상태 변경을 감지하고 컴포넌트를 리렌더링합니다.
+*/
---
+++ 14_zustand/src/App.js
@@ -0,0 +1,27 @@
+// counterStore를 import하여 사용
+// zustand의 스토어는 훅과 유사한 사용법을 가지고 있어서 use로 네이밍을 부여할 수 있습니다.
+import useCounterStore from './stores/counterStore';
+
+function App() {
+
+  // useCounterstore()
+  // 객체 구조 분해 할당을 이용해서 필요한 상태나 액션을 선택적으로 활용할 수 있습니다.
+  const { number, increment, decrement, reset } = useCounterStore();
+
+  // 선택적 활용 예시
+  // const number = useCounterStore(state => state.number);
+  // const increment = useCounterStore(state => state.increment);
+
+  return (
+    <div className="App">
+      <h1>상태 number: {number}</h1>
+
+{/* 스토어에서 정의한 액션 함수 호출 */}
+      <button onClick={() => increment()}> + </button>
+      <button onClick={decrement}> - </button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+}
+
+export default App;
